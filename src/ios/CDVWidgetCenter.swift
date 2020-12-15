4459223977ddcc84dd59958ca51d9a95b02e3cd8
Nicolas <nicolas@asphalt.run>
2020-12-15 14:41:29 +0100

    Update CDVWidgetCenter.swift

--- src/ios/CDVWidgetCenter.swift
+++ src/ios/CDVWidgetCenter.swift
@@ -1,12 +1,16 @@
+import WidgetKit
+
 @objc(CDVWidgetCenter) class CDVWidgetCenter : CDVPlugin {
   @objc(reloadAllTimelines:) // Declare your function name.
   func reloadAllTimelines(command: CDVInvokedUrlCommand) {
+    debugPrint(command)
+    let pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "Plugin Failed");
 
-    let pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
-    // Set the plugin result to succeed.
-        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
+    let res = WidgetCenter.shared.reloadAllTimelines()
+    debugPrint(res)
+    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Plugin succeeded");
     // Send the function result back to Cordova.
     self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
-		
+
   }
 }
