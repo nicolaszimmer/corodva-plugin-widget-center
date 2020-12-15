af0bfb7462b783b42f6ee33dce7b0dd376a44d76
Nicolas <nicolas@asphalt.run>
2020-12-15 15:41:43 +0100

    Update CDVWidgetCenter.swift

--- src/ios/CDVWidgetCenter.swift
+++ src/ios/CDVWidgetCenter.swift
@@ -1,16 +1,36 @@
+import Foundation
 import WidgetKit
 
-@objc(CDVWidgetCenter) class CDVWidgetCenter : CDVPlugin {
-  @objc(reloadAllTimelines:) // Declare your function name.
-  func reloadAllTimelines(command: CDVInvokedUrlCommand) {
-    debugPrint(command)
-    let pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "Plugin Failed");
+@objc(CDVWidgetCenter)
+public class CDVWidgetCenter : CDVPlugin {
 
-    let res = WidgetCenter.shared.reloadAllTimelines()
-    debugPrint(res)
-    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Plugin succeeded");
-    // Send the function result back to Cordova.
-    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+  @objc
+  func reloadTimelines(_ command: CDVInvokedUrlCommand) {
+    let kind = command.argument(at: 0) as! String?
+    let pluginResult:CDVPluginResult
+		let res = WidgetCenter.shared.reloadTimelines(ofKind kind)
+		debugPrint(res)
+    if res != nil {
+        pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: res)
+    } else {
+        pluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR)
+    }
 
+    self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
   }
+
+	@objc
+	func reloadAllTimelines(_ command: CDVInvokedUrlCommand) {
+		let pluginResult:CDVPluginResult
+		 let res = WidgetCenter.shared.reloadAllTimelines()
+		 debugPrint(res)
+			if res != nil {
+					pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: res)
+			} else {
+					pluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR)
+			}
+
+		self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+	}
+
 }
