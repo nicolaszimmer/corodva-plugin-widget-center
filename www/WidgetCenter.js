a2a983c87267047f3572d3c65692ad6d11adbba8
Nicolas <nicolas@asphalt.run>
2020-12-16 06:26:16 +0100

    add .gitignore

--- www/WidgetCenter.js
+++ www/WidgetCenter.js
@@ -1,5 +1,6 @@
 const exec = require('cordova/exec');
 const PLUGIN_NAME = "WidgetCenter";
+
 var WidgetCenter = function() {};
 WidgetCenter.reloadAllTimelines = function(onSuccess, onError) {
 	exec(onSuccess, onError, PLUGIN_NAME, "reloadAllTimelines", []);
