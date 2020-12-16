15790b634fe17aa2775b8ad866b55112946a739d
Nicolas <nicolas@asphalt.run>
2020-12-16 15:01:18 +0100

    add a readme

---
+++ README.md
@@ -0,0 +1,46 @@
+
+# cordova-plugin-widget-center
+
+**This plugin gives you access to the WidgetCenter introduced in iOS14. Use it to retrieve information about widgets that extend your Cordova app or reload their timelines (i.e. dynamic content)**
+
+Installation:
+
+```
+cordova plugin add corodova-plugin-widget-center
+```
+
+This plugin was written in Swift and requires Cordova iOS 5.0.0+ or something like `cordova-plugin-add-swift-support` to properly build.
+
+## Usage
+
+For general advice, please refer to Apple's developer information on [WidgetCenter](https://developer.apple.com/documentation/widgetkit/widgetcenter) and [creating widgets](https://developer.apple.com/documentation/widgetkit/creating-a-widget-extension) in your Xcode project.
+
+The following documentation is adapted to the plugin use from the official docs for your convenience:
+
+### Getting Configured Widget Information
+
+To get a list of user-configured widgets, use [`WidgetCenter.getCurrentConfigurations()`](https://developer.apple.com/documentation/widgetkit/widgetcenter/getcurrentconfigurations(_:)). The retun value conatins the following information:
+
+* The `kind` string matches the parameter you use when creating the widget's [`StaticConfiguration`](https://developer.apple.com/documentation/widgetkit/staticconfiguration) or [`IntentConfiguration`](https://developer.apple.com/documentation/widgetkit/intentconfiguration).
+
+* The `family` property matches one of the options specified in the `supportedFamilies` property of the widget's configuration.
+
+* If your widget is based on [`IntentConfiguration`](https://developer.apple.com/documentation/widgetkit/intentconfiguration), the `configuration` property provides the custom intent containing the user-customized values for each individual widget.
+
+The return values are only concenated into a string at the moment in this version of the plugin.
+
+### Requesting a Reload of Your Widget's Timeline
+
+Changes in your app's state may affect a widget's timeline. When this happens, you can tell WidgetKit to reload the timeline for either a specific kind of widget or all widgets. For example, your app might register for push notifications based on the widgets the user has configured. When your app receives a push notification that changes the state for one or more of your widgets, requesting a reload of their timelines updates their display.
+
+If you only need to reload a certain kind of widget, you can request a reload for only that kind. For example, in response to a push notification about a change in a game's status, you could request a reload for only the game status widgets:
+
+```
+WidgetCenter.reloadTimelines("com.mygame.gamestatus")
+```
+
+To request a reload for all of your widgets:
+
+```
+WidgetCenter.reloadAllTimelines()
+```
--- src/ios/CDVWidgetCenter.swift
+++ src/ios/CDVWidgetCenter.swift
@@ -5,55 +5,51 @@ import WidgetKit
 
 public class CDVWidgetCenter : CDVPlugin {
 
-  func reloadTimelines(command: CDVInvokedUrlCommand) {
-    let kind = command.argument(at: 0) as! String
-    var pluginResult:CDVPluginResult
-		debugPrint("reloadTimelines ofKind "+kind)
-		if #available(iOS 14, *) {
-			debugPrint("available")
-				WidgetCenter.shared.reloadTimelines(ofKind: kind)
-        pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: "Timeline reloaded")
-    } else {
-        pluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR, messageAs: "This plugin requires iOS 14 +")
+    @objc(reloadTimelines:)
+    func reloadTimelines(command: CDVInvokedUrlCommand) {
+        let kind = command.argument(at: 0) as! String
+        var pluginResult:CDVPluginResult
+        if #available(iOS 14, *) {
+            WidgetCenter.shared.reloadTimelines(ofKind: kind)
+            pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: "Timeline reloaded")
+        } else {
+            pluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR, messageAs: "This plugin requires iOS 14 +")
+        }
+
+        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
     }
 
-    self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-  }
+    @objc(reloadAllTimelines:)
+    func reloadAllTimelines(command: CDVInvokedUrlCommand) {
+        var pluginResult:CDVPluginResult
+        if #available(iOS 14, *) {
+            WidgetCenter.shared.reloadAllTimelines()
+            pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: "All Timelines reloaded")
+        } else {
+            pluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR, messageAs: "This plugin requires iOS 14 +")
+        }
+        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+    }
 
-	func reloadAllTimelines(command: CDVInvokedUrlCommand) {
-		var pluginResult:CDVPluginResult
-		debugPrint("reloadAllTimelines")
-		if #available(iOS 14, *) {
-					debugPrint("available")
- 					WidgetCenter.shared.reloadAllTimelines()
-					pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: "All Timelines reloaded")
-			} else {
-					pluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR, messageAs: "This plugin requires iOS 14 +")
-			}
-		self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-	}
-
-	func getCurrentConfigurations(command: CDVInvokedUrlCommand) {
-		debugPrint("getCurrentConfigurations")
-		if #available(iOS 14, *) {
-					debugPrint("available")
-					WidgetCenter.shared.getCurrentConfigurations { result in
-				    guard case .success(let widgets) = result else {
-							var pluginResult:CDVPluginResult
-							pluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR, messageAs: "Unable to retrieve WidgetInfo")
-							self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-							return
-						}
-						debugPrint("retrieved widgets")
-						var pluginResult:CDVPluginResult
-						pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: widgets)
-						self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-					}
-			} else {
-					var pluginResult:CDVPluginResult
-					pluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR, messageAs: "This plugin requires iOS 14 +")
-					self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
-			}
-	}
+    @objc(getCurrentConfigurations:)
+    func getCurrentConfigurations(command: CDVInvokedUrlCommand) {
+        if #available(iOS 14, *) {
+            WidgetCenter.shared.getCurrentConfigurations { result in
+                guard case .success(let widgets) = result else {
+                    var pluginResult:CDVPluginResult
+                    pluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR, messageAs: "Unable to retrieve WidgetInfo")
+                    self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+                    return
+                }
+                var pluginResult:CDVPluginResult
+                pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: String(reflecting: widgets))
+                self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+            }
+        } else {
+            var pluginResult:CDVPluginResult
+            pluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR, messageAs: "This plugin requires iOS 14 +")
+            self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+        }
+    }
 
 }
